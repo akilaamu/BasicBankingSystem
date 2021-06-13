0ebfb2fb0bc94672a399b12c3fd2b780df934cca
akilaamu <80111012+akilaamu@users.noreply.github.com>
2021-06-13 00:05:04 -0700

    Update transactionhistory.php

--- transactionhistory.php
+++ transactionhistory.php
@@ -6,12 +6,12 @@
     <title>Transaction History</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/table.css">
-    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.html">
 </head>
 <body style="background-color:moccasin">
 
 <?php
-  include 'navbar.php';
+  include 'navbar.html';
 ?>
 
 	<div class="container">
